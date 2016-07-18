5e22bbfb96cdf7de4cd1986e9615a0964da6608c
Johannes Seikowsky <seikowsky@googlemail.com>
2016-07-18 04:43:38 +0300

    shipping profile_image file type and size validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,4 +6,16 @@ class Profile < ActiveRecord::Base
   # adding carrierwave
   mount_uploader :profile_image, ProfileImageUploader
 
+  # making sure ppl don't upload huge files
+  validate  :profile_image_size
+
+  private
+
+  # custom validation method
+  def profile_image_size
+    if profile_image.size > 4.megabytes
+      errors.add(:profile_image, "Image too big. Please upload a smaller one.")
+    end
+  end
+
 end
